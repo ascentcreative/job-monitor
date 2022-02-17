fb83cfa138d175e670fe2ce6fa9cb3571fc8c90b
Kiersm <kieran@ascent-creative.co.uk>
2022-02-17 14:16:08 +0000

    Timeout Polling

--- resources/views/components/monitorprogress.blade.php
+++ resources/views/components/monitorprogress.blade.php
@@ -7,6 +7,17 @@
     <div class="progress-bar" id="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
 </div>
 
+
+{{-- 
+    
+    Need to update this script.
+
+    Currently, if the freq is too low, it will fire off requests before the previous ones have completed, causing a cascade of processes on the server.
+
+    Instead, it should poll, wait for a response and then set a short TimeOut (single shot) which polls again.
+    
+    
+    --}}
 <script>
 
     function poll() {
@@ -18,23 +29,29 @@
             $('#progress-number').html( Math.round(data.amount_completed, 2) + "%");
 
             if (data.is_complete == 1) {
-                window.clearInterval($handle);
+               // window.clearInterval($handle);
                 $('body').trigger('job_complete');
+                return;
             }
 
             if (data.is_error == 1) {
-                window.clearInterval($handle);
+               // window.clearInterval($handle);
                 $('body').trigger('job_failed');
+                return;
             }
+
+            // no completion messages, re-poll in 100ms;
+            window.setTimeout(poll, 100);
 
 
         });
 
     }
 
-    $handle = window.setInterval(() => {
-        poll();
-    }, {{ $freq ?? 250 }});
+    // $handle = window.setInterval(() => {
+    // $handle = wind
+        // poll();
+    //}, {{ $freq ?? 250 }});
 
     poll();
 
