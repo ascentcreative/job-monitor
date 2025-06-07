ad825b09daa7137a767223d9fcc0a8f5ee9f2003
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2025-06-07 21:38:01 +0100

    Update to trigger refresh

--- resources/views/modal/monitor.blade.php
+++ resources/views/modal/monitor.blade.php
@@ -25,25 +25,6 @@
 
 @endsection --}}
 
-<x-cms-modal modalId="ajaxModal" size="sm"
-    :backdropclose="false" :closebutton="false" :keyboardclose="false"
-    :showHeader="true" :showFooter="false" :fade="false"
-
-    >
-
-    <x-slot name="title">
-        {{ $job_title ?? "Progress"}}
-    </x-slot>
-
-    <div class="working">    
-
-        <x-jobmonitor-progress monitorid="{{ $monitor_id }}" freq="{{ $freq ?? 500 }}" />
-        
-    </div>
-
-</x-cms-modal>
-
-
 
 
 @push('scripts')
@@ -67,3 +48,45 @@
 </script>
 
 @endpush
+
+@push('modal-scripts')
+
+<script>
+
+    $('#ajaxModal').on('hide.bs.modal', function (e) {
+        window.location.reload();
+    });
+
+    // $('body').on('job_complete', function() {
+    //     $('#ajaxModal .working').hide();
+    //     $('#ajaxModal .complete').show();
+    // });
+
+    // $('body').on('job_failed', function() {
+    //     $('#ajaxModal .working').hide();
+    //     $('#ajaxModal .failed').show();
+    // });
+
+</script>
+
+@endpush
+
+<x-cms-modal modalId="ajaxModal" size="sm"
+    :backdropclose="false" :closebutton="false" :keyboardclose="false"
+    :showHeader="true" :showFooter="false" :fade="false"
+
+    >
+
+    <x-slot name="title">
+        {{ $job_title ?? "Progress"}}
+    </x-slot>
+
+    <div class="working">    
+
+        <x-jobmonitor-progress monitorid="{{ $monitor_id }}" freq="{{ $freq ?? 500 }}" />
+        
+    </div>
+
+</x-cms-modal>
+
+
